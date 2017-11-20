3b4bb7631cba897afe137a9238702d868954060c
Yoohoo <yao460733480@outlook.com>
2017-11-20 19:03:22 +0800

    initial

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return <div className="App">帆帆function</div>;
+  }
+}
+
+App.propTypes = {};
+
+App.defaultProps = {};
+
+export default App;
